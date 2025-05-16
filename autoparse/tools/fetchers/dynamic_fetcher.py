32d130a26157112e716e07504b4d0bc94cd843be
Denigmma <->
2025-05-16 20:35:31 +0300

    edit dynamic fetcher

--- autoparse/tools/fetchers/dynamic_fetcher.py
+++ autoparse/tools/fetchers/dynamic_fetcher.py
@@ -29,7 +29,9 @@ def fetch_dynamic_html(
     """
     options = Options()
     if headless:
-        options.add_argument("--headless")
+        options.add_argument("--headless=new")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-software-rasterizer")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
